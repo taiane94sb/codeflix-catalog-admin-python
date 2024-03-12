6f6c6d7b99e8e6b32ffd2a3cf46d1c9caa1977f8
taiane94sb <taiane19sb94@gmail.com>
2024-03-12 18:04:47 -0300

    configurando pytest

--- test_category.py
+++ test_category.py
@@ -1,4 +1,4 @@
-import unittest
+import pytest
 
 from uuid import UUID
 import uuid
@@ -6,32 +6,28 @@ import uuid
 from category import Category
 
 
-class TestCategory(unittest.TestCase):
-    def test_name_is_required_1(self):
-        with self.assertRaises(TypeError):
-            Category()
-
-    def test_name_is_required_2(self):
-        with self.assertRaisesRegex(TypeError, "missing 1 required positional argument: 'name'"):
+class TestCategory:
+    def test_name_is_required(self):
+        with pytest.raises(TypeError,match="missing 1 required positional argument: 'name'"):
             Category()
 
     def test_name_must_have_less_than_256_characters(self):
-        with self.assertRaisesRegex(ValueError, "name must have less than 256 characters"):
+        with pytest.raises(ValueError, match="name must have less than 256 characters"):
             Category(name="a" * 256)
 
     def test_category_must_be_created_with_id_as_uuid_by_default(self):
         category = Category(name="Filme")
-        self.assertEqual(type(category.id), UUID)
+        assert isinstance(category.id, UUID)
     
     def test_create_category_with_default_values(self):
         category = Category(name="Filme")
-        self.assertEqual(category.name, "Filme")
-        self.assertEqual(category.description, "")
-        self.assertEqual(category.is_active, True)
+        assert category.name == "Filme"
+        assert category.description == ""
+        assert category.is_active is True
 
     def test_create_category_as_active_by_default(self):
         category = Category(name="Filme")
-        self.assertEqual(category.is_active, True)
+        assert category.is_active is True
 
     def test_create_category_with_provided_values(self):
         cat_id = uuid.uuid4()
@@ -41,11 +37,7 @@ class TestCategory(unittest.TestCase):
             description="Filmes em geral",
             is_active=False,
         )
-        self.assertEqual(category.id, cat_id)
-        self.assertEqual(category.name, "Filme")
-        self.assertEqual(category.description, "Filmes em geral")
-        self.assertEqual(category.is_active, False)
-
-
-if __name__ == "__main__":
-    unittest.main()
+        assert category.id == cat_id
+        assert category.name == "Filme"
+        assert category.description == "Filmes em geral"
+        assert category.is_active is False
