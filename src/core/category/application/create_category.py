bbfe2536d2633ac107fcb08d615d366b5ea7f434
taiane94sb <taiane19sb94@gmail.com>
2024-03-13 17:24:28 -0300

    caso de uso create category

---
+++ src/core/category/application/create_category.py
@@ -0,0 +1,14 @@
+from uuid import UUID
+
+from core.category.domain.category import Category
+
+
+def create_category(name: str, description: str, is_active: bool = True) -> UUID:
+    category = Category(
+        name=name,
+        description=description,
+        is_active=is_active
+    )
+
+    return category.id
+
