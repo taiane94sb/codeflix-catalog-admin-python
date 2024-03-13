5d8f6af308d5eb69f1c27bac1f7676b28321e91a
taiane94sb <taiane19sb94@gmail.com>
2024-03-13 14:08:47 -0300

    refatoração com dataclasses

--- category.py
+++ category.py
@@ -1,19 +1,33 @@
 import uuid
 
-class Category:
-    def __init__(
-        self,
-        name,
-        id = "",
-        description = "",
-        is_active = True,
-        ):
-            self.id = id or uuid.uuid4()
-            self.name = name
-            self.description = description
-            self.is_active = is_active
+from uuid import UUID
 
-            self.validate()
+from dataclasses import dataclass, field
+
+
+@dataclass
+class Category:
+    name: str
+    description: str = ""
+    is_active: bool = True
+    id: UUID = field(default_factory=uuid.uuid4)
+
+    def __post_init__(self):
+        self.validate()
+
+    # def __init__(
+    #     self,
+    #     name,
+    #     id = "",
+    #     description = "",
+    #     is_active = True,
+    #     ):
+    #         self.id = id or uuid.uuid4()
+    #         self.name = name
+    #         self.description = description
+    #         self.is_active = is_active
+
+    #         self.validate()
             
     def validate(self):
         if len(self.name) > 255:
@@ -31,7 +45,7 @@ class Category:
     def __eq__(self, other):  # a == b => a.__eq__(b)
         if not isinstance(other, Category):
             return False
-                
+
         return self.id == other.id
 
     def update_category(self, name, description):
