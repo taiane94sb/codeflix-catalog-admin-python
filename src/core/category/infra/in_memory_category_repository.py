6ae980b2e9ff0ea90961da3d6da2c571093ca092
taiane94sb <taiane19sb94@gmail.com>
2024-03-24 08:50:08 -0300

    utilizando um repositório

---
+++ src/core/category/infra/in_memory_category_repository.py
@@ -0,0 +1,7 @@
+class InMemoryCategoryRepository:
+    def __init__(self, categories=None):
+        self.categories = categories or []
+
+    def save(self, category):
+        self.categories.append(category)
+    
--- src/core/category/tests/application/test_create_category.py
+++ src/core/category/tests/application/test_create_category.py
@@ -1,14 +1,21 @@
 import pytest
 
+from unittest.mock import MagicMock
+
 from uuid import UUID
 import uuid
 
 from src.core.category.application.create_category import create_category, InvalidCategoryData
+from src.core.category.domain.category import Category
+from src.core.category.infra.in_memory_category_repository import InMemoryCategoryRepository
 
 
 class TestCreateCategory:
     def test_create_category_with_valid_data(self):
+        mock_repository = MagicMock(InMemoryCategoryRepository)
+
         category_id = create_category(
+            repository=mock_repository,
             name="Filme", 
             description="Categoria",
             is_active=True
@@ -16,6 +23,7 @@ class TestCreateCategory:
 
         assert category_id is not None
         assert isinstance(category_id, UUID)
+        assert mock_repository.save.called is True
     
     def test_create_category_with_invalid_data(self):
         with pytest.raises(InvalidCategoryData, match="name cannot be empty") as exc_info:
